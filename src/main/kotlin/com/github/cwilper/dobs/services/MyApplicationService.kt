69df3f10f6c5e63fb7bd0341e12a2b82b055142a
GitHub Action <action@github.com>
2021-04-03 15:06:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/cwilper/dobs/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cwilper/dobs/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cwilper.dobs.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cwilper.dobs.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/cwilper/dobs/services/MyProjectService.kt
+++ src/main/kotlin/com/github/cwilper/dobs/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.cwilper.dobs.services
 
+import com.github.cwilper.dobs.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
